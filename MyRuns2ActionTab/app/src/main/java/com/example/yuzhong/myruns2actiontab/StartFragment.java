85f5e5552d571d7239023a6009b88d7488b19eb1
cuteQ <cuteqianyuzhong@gmail.com>
2016-04-08 15:35:36 -0400

    Update StartFragment.java

--- MyRuns2ActionTab/app/src/main/java/com/example/yuzhong/myruns2actiontab/StartFragment.java
+++ MyRuns2ActionTab/app/src/main/java/com/example/yuzhong/myruns2actiontab/StartFragment.java
@@ -1,8 +1,8 @@
 package com.example.yuzhong.myruns2actiontab;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,42 +11,46 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
+/**
+ * Created by Yuzhong on 2016/4/7.
+ */
+public class StartFragment extends Fragment{
+    private Button mButtonStart;
+    private Button mSyncButton;
+    private Spinner mInputspinner;
+    private Spinner mActivityspinner;
 
-public class StartFragment extends Fragment {
-
-    //    public static final int MANUL = 0;
-//    public static final int GPS =1;
-//    public static final int AUTOMATIC = 1;
-    private Button buttonStart;
-    private Button SyncButton;
-    private Spinner Inputspinner;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     */
-    // TODO: Rename and change types and number of parameters
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_start, container, false);
+        View view =  inflater.inflate(R.layout.start_fragment
+                , container, false);
 
-        Inputspinner = (Spinner) view.findViewById(R.id.spinner);
+        mInputspinner = (Spinner) view.findViewById(R.id.input_type);
 
         ArrayAdapter<CharSequence> inputAdapter = ArrayAdapter.createFromResource(view.getContext(),
+                R.array.InputTypeSpinner, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        inputAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        mInputspinner.setAdapter(inputAdapter);
+
+        mActivityspinner = (Spinner) view.findViewById(R.id.activity_type);
+
+        ArrayAdapter<CharSequence> activityAdapter = ArrayAdapter.createFromResource(view.getContext(),
                 R.array.ActivityTypeSpinner, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         inputAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
-        Inputspinner.setAdapter(inputAdapter);
-        buttonStart = (Button) view.findViewById(R.id.StartButton);
-        buttonStart.setOnClickListener(new View.OnClickListener() {
+        mActivityspinner.setAdapter(activityAdapter);
+
+        mButtonStart = (Button) view.findViewById(R.id.StartButton);
+        mButtonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent();
-                Log.d("CS65",Inputspinner.getSelectedItem().toString());
-                if (Inputspinner.getSelectedItem().toString().equals("Manual Entry")) {
+                Intent i;
+                Log.d("CS65", mInputspinner.getSelectedItem().toString());
+                if (mInputspinner.getSelectedItem().toString().equals("Manual Entry")) {
                     i = new Intent(getActivity(),ManualActivity.class);
                 }
 
@@ -59,6 +63,4 @@ public class StartFragment extends Fragment {
         });
         return view;
     }
-
-
 }
